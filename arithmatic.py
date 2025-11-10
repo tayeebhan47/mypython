d7b3302bd263b1f6d858e8b7585eaa0b2af71373
tayeebhan47 <khantayeeb937@gmail.com>
2025-11-10 16:29:00 +0530

    Create arithmatic.py

---
+++ arithmatic.py
@@ -0,0 +1,10 @@
+a=int(input("enter the number"))
+b=int(input("enter the number"))
+#addition
+sum1=a+b 
+sum2=a-b 
+sum3=a/b 
+sum4=a//b
+sum5=a%b 
+sum6=a*b
+print("addition is",sum1,sum2,sum3,sum4,sum5)
