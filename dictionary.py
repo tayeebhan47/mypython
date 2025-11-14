fc2559e2220df372c40740a62101813eda31f0ac
tayeebhan47 <khantayeeb937@gmail.com>
2025-11-14 11:16:29 +0530

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+#eb)dictionary
+tayeeb={
+    "name"  :"khan",
+    "age"   :20,
+    "weight":60.5
+}
+print(tayeeb)
+tayeeb["date"]=17.10
+print(tayeeb)
+del tayeeb["age"]
+print(tayeeb)
+for i in tayeeb:
+  print(i,tayeeb)
