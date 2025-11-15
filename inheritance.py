a614178e36a240a0953f520c30f1f4bb67d0f41f
tayeeb khan 47 <khantayeeb937@gmail.com>
2025-11-15 11:59:14 +0530

    Create inheritance.py

---
+++ inheritance.py
@@ -0,0 +1,11 @@
+#inheritance
+class father:
+  def function(self):
+    print("he is good")
+class mother:
+  def function(self):
+    print("she is good")
+obj=father()
+cell=mother()
+obj.function()
+cell.function()        
